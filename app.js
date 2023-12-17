2c2c50d79ba106b1db6cd9f5780be1ca2fb4f60e
keito-klein <taufikqillah25@gmail.com>
2023-12-17 18:24:11 +0700

    add ejs-layouts

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts")
 const app = express()
 const port = 3000
 
 //Using View Engine ejs
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
+//get Root address
 app.get('/', (req, res) => {
-	res.render("root", { title: "1001-mp3" });
+	res.render("root",
+		{ title: "1001-mp3",
+		  layout: "layout/main-layout"
+		});
+})
+
+
+app.use('/', (req, res) => {
+	res.status(404)
+	res.send('<h1> 404 </h1>')
 })
 
 app.listen(port, () => {
- console.log("System Uptime!")
+ console.log("System Uptime!");
 })
